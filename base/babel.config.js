9508c8c2c4eb8517dcf8aaaf5062086ccf3167de
tu.van <29675175+congtuuit@users.noreply.github.com>
2025-01-15 15:25:02 +0700

    push js code base on https://github.com/thisisqubika/react-native-template.git

--- base/babel.config.js
+++ base/babel.config.js
@@ -1,3 +1,14 @@
+const alias = { '^@/(.+)': './src/\\1' }; // @/folder will be an alias to <root>/src/folder
+const extensions = ['.android.js', '.ios.js', '.js', '.json', '.native'];
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [['module-resolver', { alias, extensions }], 'react-native-reanimated/plugin'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
+
+
